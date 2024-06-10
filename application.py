9f85610036d0fac85cfd2f0ccde76172aa727707
anupam <anupamkumar7802@gmail.com>
2024-06-10 22:57:04 +0530

    docker file edited

--- application.py
+++ application.py
@@ -18,23 +18,23 @@ app=application
 
 # Route for a home page
 
-try:
+# try:
     
-    data_ingetion= DataIngestion()
-    train_data_path, test_data_path =data_ingetion.initiate_data_ingestion()
+#     data_ingetion= DataIngestion()
+#     train_data_path, test_data_path =data_ingetion.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    train_arr, test_arr, _=data_transformation.initiate_data_transormation(train_data_path, test_data_path )
+#     data_transformation = DataTransformation()
+#     train_arr, test_arr, _=data_transformation.initiate_data_transormation(train_data_path, test_data_path )
 
-    model_trainer=ModelTrainer()
-    print(model_trainer.initiate_model_trainer( train_arr,test_arr))
+#     model_trainer=ModelTrainer()
+#     print(model_trainer.initiate_model_trainer( train_arr,test_arr))
 
     
     
 
 
-except Exception as e:
-    raise CustomException(e,sys)
+# except Exception as e:
+#     raise CustomException(e,sys)
 
 
 
@@ -95,7 +95,7 @@ def predict_using_api():
 
 
 if __name__=="__main__":
-    app.run()      
+    app.run(host="0.0.0.0",port=5000)      
 
 
 
